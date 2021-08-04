e7e126f1a4b96287d0c4c3a429b2284a8c14cb48
Joey <30815220+joeylking@users.noreply.github.com>
2021-08-03 22:59:49 -0500

    game functions

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 import Deck from "./deck.js";
 
+const CARD_VALUE_MAP = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
 const computerCardSlot = document.querySelector(".computer-card-slot");
 const playerCardSlot = document.querySelector(".player-card-slot");
 const computerDeckElement = document.querySelector(".computer-deck");
 const playerDeckElement = document.querySelector(".player-deck");
 const text = document.querySelector(".text");
 
-let playerDeck, computerDeck, inRound;
+let playerDeck, computerDeck, inRound, stop;
 
 document.addEventListener("click", () => {
+  if (stop) {
+    startGame();
+    return;
+  }
+
   if (inRound) {
     reset();
   } else {
@@ -25,6 +46,7 @@ function startGame() {
   playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
   computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
   inRound = false;
+  stop = false;
 
   reset();
 }
@@ -46,9 +68,41 @@ function flipCards() {
 
   playerCardSlot.appendChild(playerCard.getHTML());
   computerCardSlot.appendChild(computerCard.getHTML());
+
+  updateDeckCount();
+
+  if (isRoundWinner(playerCard, computerCard)) {
+    text.innerText = "Win";
+    playerDeck.push(playerCard);
+    playerDeck.push(computerCard);
+  } else if (isRoundWinner(computerCard, playerCard)) {
+    text.innerText = "Lose";
+    computerDeck.push(playerCard);
+    computerDeck.push(computerCard);
+  } else {
+    text.innerText = "Draw";
+    playerDeck.push(playerCard);
+    computerDeck.push(computerCard);
+  }
+
+  if (isGameOver(playerDeck)) {
+    text.innerText = "You Lose!";
+    stop = true;
+  } else if (isGameOver(computerDeck)) {
+    text.innerText = "You Win!";
+    stop = true;
+  }
 }
 
 function updateDeckCount() {
   computerDeckElement.innerText = computerDeck.numberOfCards;
   playerDeckElement.innerText = playerDeck.numberOfCards;
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+  return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value];
+}
+
+function isGameOver(deck) {
+  return deck.numberOfCards === 0;
 }
